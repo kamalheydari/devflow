e1e096c83dd14fec7a9b877a2f8dbc99219b62cf
kamal heydari <kamal3heydari@gmail.com>
2023-11-04 16:10:13 +0330

    create routes

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,3 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <main className="flex-center min-h-screen w-full">{children}</main>
+}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className='flex p-2 '>hello world</div>
-}
